48669ea63d1c68f1da99ee5301dc93958cb7861a
prenaissance <alexandru.andries@isa.utm.md>
2022-02-09 00:36:31 +0200

    asdf

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,7 +6,9 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-
+            int j = 33;
+            j *= 4;
+            Console.WriteLine(j);
             Console.WriteLine("Hello World!");
             Class1.Meow();
             int i = 2345;
